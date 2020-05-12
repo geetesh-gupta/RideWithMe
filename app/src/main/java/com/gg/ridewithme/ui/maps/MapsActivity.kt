c9776fd4ae7773c830ce9ed95c28a95eef8b91b1
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-05-12 21:34:42 +0530

    Replacce MainActivity with MapsActivity

--- app/src/main/java/com/gg/ridewithme/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/gg/ridewithme/ui/maps/MapsActivity.kt
@@ -1,12 +1,13 @@
-package com.gg.ridewithme
+package com.gg.ridewithme.ui.maps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gg.ridewithme.R
 
-class MainActivity : AppCompatActivity() {
+class MapsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_maps)
     }
 }
