01acf330b665007635763e56efa107f1ddf0c0be
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-05-12 22:38:29 +0530

    Add Permissions in MapsActivity

--- app/src/main/java/com/gg/ridewithme/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/gg/ridewithme/ui/maps/MapsActivity.kt
@@ -1,17 +1,29 @@
 package com.gg.ridewithme.ui.maps
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.gg.ridewithme.R
+import com.gg.ridewithme.data.network.NetworkService
+import com.gg.ridewithme.utils.PermissionUtils
 import com.gg.ridewithme.utils.ViewUtils
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
+
+    companion object {
+        private const val TAG = "MapsActivity"
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 999
+        private const val PICKUP_REQUEST_CODE = 1
+        private const val DROP_REQUEST_CODE = 2
+    }
 
     private lateinit var mMap: GoogleMap
+    private lateinit var presenter: MapsPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +32,66 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        presenter = MapsPresenter(NetworkService())
+        presenter.onAttach(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+    }
+
+    override fun onStart() {
+        super.onStart()
+        when {
+            PermissionUtils.isAccessFineLocationGranted(this) -> {
+                when {
+                    PermissionUtils.isLocationEnabled(this) -> {
+
+                    }
+                    else -> {
+                        PermissionUtils.showGPSNotEnabledDialog(this)
+                    }
+                }
+            }
+            else -> {
+                PermissionUtils.requestAccessFineLocationPermission(
+                    this,
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    when {
+                        PermissionUtils.isLocationEnabled(this) -> {
+
+                        }
+                        else -> {
+                            PermissionUtils.showGPSNotEnabledDialog(this)
+                        }
+                    }
+                } else {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.location_permission_not_granted),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.onDetach()
     }
 }
