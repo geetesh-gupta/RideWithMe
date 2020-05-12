9cd6a140e14ba83ded867852191d4d60bcab9731
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-05-12 23:07:30 +0530

    Add locationListener to MapsActivity

--- app/src/main/java/com/gg/ridewithme/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/gg/ridewithme/ui/maps/MapsActivity.kt
@@ -3,14 +3,22 @@ package com.gg.ridewithme.ui.maps
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.widget.Toast
 import com.gg.ridewithme.R
 import com.gg.ridewithme.data.network.NetworkService
 import com.gg.ridewithme.utils.PermissionUtils
 import com.gg.ridewithme.utils.ViewUtils
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 
 
 class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
@@ -22,8 +30,12 @@ class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
         private const val DROP_REQUEST_CODE = 2
     }
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var googleMap: GoogleMap
     private lateinit var presenter: MapsPresenter
+
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    private lateinit var locationCallback: LocationCallback
+    private var currentLatLng: LatLng? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,8 +48,52 @@ class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
         presenter.onAttach(this)
     }
 
+    private fun moveCamera(latLng: LatLng?) {
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+    }
+
+    private fun animateCamera(latLng: LatLng?) {
+        val cameraPosition = CameraPosition.Builder().target(latLng).zoom(15.5f).build()
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+    }
+
+    private fun enableMyLocationOnMap() {
+        googleMap.setPadding(0, ViewUtils.dpToPx(48f), 0, 0)
+        googleMap.isMyLocationEnabled = true
+    }
+
+    private fun setupLocationsListener() {
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+        val locationRequest = LocationRequest().setInterval(2000).setFastestInterval(2000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                if (currentLatLng == null) {
+                    for (location in locationResult.locations) {
+                        if (currentLatLng == null) {
+                            currentLatLng = LatLng(location.latitude, location.longitude)
+                            enableMyLocationOnMap()
+                            moveCamera(currentLatLng)
+                            animateCamera(currentLatLng)
+                        }
+                    }
+                }
+                // Few more things we can do here:
+                // For example: Update the location of user on server
+            }
+        }
+
+        fusedLocationProviderClient?.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.myLooper()
+        )
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        this.googleMap = googleMap
     }
 
     override fun onStart() {
@@ -46,7 +102,7 @@ class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
             PermissionUtils.isAccessFineLocationGranted(this) -> {
                 when {
                     PermissionUtils.isLocationEnabled(this) -> {
-
+                        setupLocationsListener()
                     }
                     else -> {
                         PermissionUtils.showGPSNotEnabledDialog(this)
@@ -73,7 +129,7 @@ class MapsActivity : AppCompatActivity(), MapsView, OnMapReadyCallback {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     when {
                         PermissionUtils.isLocationEnabled(this) -> {
-
+                            setupLocationsListener()
                         }
                         else -> {
                             PermissionUtils.showGPSNotEnabledDialog(this)
