90d0e56189d6af92298399b8a686d38aea142448
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-05-12 22:17:23 +0530

    Setup MapsActivity and related libraries

---
+++ app/src/main/java/com/gg/ridewithme/RideWithMe.kt
@@ -0,0 +1,17 @@
+package com.gg.ridewithme
+
+import android.app.Application
+import com.google.android.libraries.places.api.Places
+import com.google.maps.GeoApiContext
+import com.mindorks.ridesharing.simulator.Simulator
+
+class RideWithMe : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Places.initialize(applicationContext, getString(R.string.google_maps_key));
+        Simulator.geoApiContext = GeoApiContext.Builder()
+            .apiKey(getString(R.string.google_maps_key))
+            .build()
+    }
+}
--- app/src/main/java/com/gg/ridewithme/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/gg/ridewithme/ui/maps/MapsActivity.kt
@@ -3,11 +3,26 @@ package com.gg.ridewithme.ui.maps
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gg.ridewithme.R
+import com.gg.ridewithme.utils.ViewUtils
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapsActivity : AppCompatActivity() {
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        ViewUtils.enableTransparentStatusBar(window)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
     }
 }
